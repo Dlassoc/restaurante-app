c7c007762358b4d8443b3d0d4fd69ff2d950657a
Daniel Lasso <you@example.com>
2026-03-04 00:25:45 -0500

    😎 creando el type de reservas

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -12,3 +12,12 @@ export type Menu = {
   precio: string | number; // Prisma Decimal puede llegar como string
   disponible: boolean;
 };
+
+export type Reserva = {
+  id: number;
+  restauranteId: number;
+  nombreCliente: string;
+  personas: number;
+  fechaHora: string; // ISO
+  notas?: string | null;
+};
